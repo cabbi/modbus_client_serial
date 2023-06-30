dbe1e1bd93d31d73cd153afd40ad18a3099d72be
fabio <fabio@cabella.net>
2023-06-30 23:12:33 +0200

    ready to publish

---
+++ example/read_request_example.dart
@@ -0,0 +1,22 @@
+import 'package:modbus_client/modbus_client.dart';
+import 'package:modbus_client_serial/modbus_client_serial.dart';
+
+void main() async {
+  // Create a modbus int16 register element
+  var batteryTemperature = ModbusInt16Register(
+      name: "BatteryTemperature",
+      type: ModbusElementType.inputRegister,
+      address: 22,
+      uom: "°C",
+      multiplier: 0.1,
+      onUpdate: (self) => print(self));
+
+  // Create the modbus client.
+  var modbusClient = ModbusClientSerialRtu(portName: "COM2", unitId: 1);
+
+  // Send a read request from the element
+  await modbusClient.send(batteryTemperature.getReadRequest());
+
+  // Ending here
+  modbusClient.disconnect();
+}
